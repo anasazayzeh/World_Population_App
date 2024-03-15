bd0cbe3451febcdebdac9da2f05f8b7041f886bc
anasazayzeh <158519171+anasazayzeh@users.noreply.github.com>
2024-03-15 12:55:51 +0300

    Update HW03App.py

--- HW03App.py
+++ HW03App.py
@@ -8,7 +8,7 @@ import numpy as np
 
 
 # Read the CSV data
-df = pd.read_csv(r"C:\Users\anas\OneDrive\المستندات\AUB MASTERS PROGRAM\ACADEMIA\DATA VISUALIZATION\ANAS AZAYZEH - HW 03\World Population Growth.csv", dtype={"Year": object})
+df = pd.read_csv(r"https://github.com/anasazayzeh/World_Population_App/blob/main/Data.csv", dtype={"Year": object})
 df = df.rename(columns={'Number': 'PopulationGrowth'})
 
 
